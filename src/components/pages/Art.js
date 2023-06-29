c3ea7da2d040f1a8d0c64617962657997571d036
Badi-Haki Martin <jojackblack@gmail.com>
2023-06-29 13:40:42 -0400

    add content to art page

--- src/components/pages/Art.js
+++ src/components/pages/Art.js
@@ -2,10 +2,17 @@ import React from "react";
 
 function Art(){
     return(
-        <div>
-            <h1>
-                Art
-            </h1>
+        <div id="art">
+            <h1>Art</h1>
+            <p className="intro">
+                A million worlds, birthed of one mind...
+            </p>
+            <p className="panel">
+            Welcome to the gallery. This is a small selection of some of my best work. To see
+            more of my work, please visit my 
+            <br />
+            <a href={"https://bhakim.artstation.com"} target="_blank" >Full Portfolio</a>.
+            </p>
         </div>
     )
 }
