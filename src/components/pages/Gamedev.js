975af375d3bcef0a42754d2b0bd64822cb56c906
Badi-Haki Martin <jojackblack@gmail.com>
2023-07-06 15:34:45 -0400

    created gamePost component, and data for 2 game projects. CSS updates for game page and gamePost component

--- src/components/pages/Gamedev.js
+++ src/components/pages/Gamedev.js
@@ -1,12 +1,36 @@
 import React from "react";
+import GamePost from "./gamedev-sub-components/GamePost";
 
 function Gamedev(){
+    const listOfGames = [
+        {
+            id:0,
+            title:"The Eldritch and the Iron",
+            description:"An auto-scrolling, mech-based shooter made in Unity3D for MechJam III",
+            itch:"https://badihaki.itch.io/the-eldritch-and-the-iron",
+            github:"https://github.com/badihaki/eldrith-iron"
+        },
+        {
+            id:1,
+            title:"2D Interactive Map",
+            description:"An interactive map template, made in the style of Super Mario Bros 3, with Unity3D and C#.",
+            itch:null,
+            github:"https://github.com/badihaki/2D-Interactive-Map"
+        }
+    ]
+
     return(
         <div className="gamedev">
             <h1>
                 Gamedev
             </h1>
-            <p className="intro"></p>
+            <p className="intro">Play in my imagination</p>
+            <p className="panel">
+                Below you'll find different releases of various prototypes, demos and other interactive ideas that I've worked on through the years.
+            </p>
+            <ul id="games-list">
+                {listOfGames.map( game=><GamePost game={game} key={game.id} /> )}
+            </ul>
         </div>
     )
 }
---
+++ src/components/pages/gamedev-sub-components/GamePost.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function GamePost({ game }){
+    return(
+        <li className="game-post">
+            <h3>{game.title}</h3>
+            <p>{game.description}</p>
+            { game.itch? <a href={game.itch} target="_blank" rel="noreferrer" >Itch.io PAge</a> : "No Itch.io Page" }
+            <br />
+            { game.github? <a href={game.github} target="_blank" rel="noreferrer" >Github Repo</a> : "No Repository" }
+        </li>
+    )
+}
+
+export default GamePost;
