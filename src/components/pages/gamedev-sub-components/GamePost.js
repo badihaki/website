91b15611c25311ad2cf088e0c344dd0348a8600b
Badi-Haki Martin <jojackblack@gmail.com>
2023-07-06 15:58:50 -0400

    updated css for gamePost component

--- src/components/pages/gamedev-sub-components/GamePost.js
+++ src/components/pages/gamedev-sub-components/GamePost.js
@@ -5,7 +5,7 @@ function GamePost({ game }){
         <li className="game-post">
             <h3>{game.title}</h3>
             <p>{game.description}</p>
-            { game.itch? <a href={game.itch} target="_blank" rel="noreferrer" >Itch.io PAge</a> : "No Itch.io Page" }
+            { game.itch? <a href={game.itch} target="_blank" rel="noreferrer" >Itch.io Page</a> : "No Itch.io Page" }
             <br />
             { game.github? <a href={game.github} target="_blank" rel="noreferrer" >Github Repo</a> : "No Repository" }
         </li>
