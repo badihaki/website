3f70d5680ac32ae45c5703f57426623943b1c754
badihaki <jojackblack@gmail.com>
2024-12-02 15:33:57 -0500

    implemented tabs on main screen

--- src/lib/Navigation/HomeIcon.tsx
+++ src/lib/Navigation/HomeIcon.tsx
@@ -7,7 +7,7 @@ function HomeIcon() {
   return (
     <div className='transition-all ease-in-out duration-700 absolute justify-center items-center w-[50px] top-2 left-4 hover:w-[100px] hover:top-4 hover:left-4'>
         <Link href={"/"}>
-            <div className='flex transition-all ease-in-out duration-500 bg-slate-300 bg-opacity-15 hover:bg-opacity-25 rounded-full justify-center items-center px-2'>
+            <div className='flex transition-all ease-in-out duration-500 bg-[#341e7a] bg-opacity-15 hover:bg-opacity-35 rounded-full justify-center items-center px-2'>
                 <Image src={icon} alt='Icon to take you home' 
                 className='max-w-[75px]'/>
             </div>
