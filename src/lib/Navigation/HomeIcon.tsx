0a1ec53d0bfbed8d3d958f9353cedef71a203f31
badihaki <jojackblack@gmail.com>
2024-12-02 15:02:40 -0500

    home button works flawlessly

--- src/lib/Navigation/HomeIcon.tsx
+++ src/lib/Navigation/HomeIcon.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
 import icon from '@/lib/_Images/banner-open.png'
+import Link from 'next/link'
 
 function HomeIcon() {
   return (
-    <div>
-        <Image src={icon} alt='Icon to take you home' />
+    <div className='transition-all ease-in-out duration-700 absolute justify-center items-center w-[50px] top-2 left-4 hover:w-[100px] hover:top-4 hover:left-4'>
+        <Link href={"/"}>
+            <div className='flex transition-all ease-in-out duration-500 bg-slate-300 bg-opacity-15 hover:bg-opacity-25 rounded-full justify-center items-center px-2'>
+                <Image src={icon} alt='Icon to take you home' 
+                className='max-w-[75px]'/>
+            </div>
+        </Link>
     </div>
   )
 }
