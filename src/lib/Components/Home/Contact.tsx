6a9031e2f4054e095b5216f0c3848c9428aac657
badihaki <jojackblack@gmail.com>
2024-12-16 18:30:36 -0500

    added key prop to li elements in ul element

--- src/lib/Components/Home/Contact.tsx
+++ src/lib/Components/Home/Contact.tsx
@@ -4,22 +4,27 @@ import React from 'react'
 
 function Contact() {
   const socials:{
+    id:number,
     text:string,
     href:string,
   }[] = [
     {
+      id:0,
       text:"LinkedIn",
       href:"https://www.linkedin.com/in/badi-haki-martin-93481531b/",
     },
     {
+      id:1,
       text:"Bluesky",
       href:"https://bsky.app/profile/kurokaras.bsky.social",
     },
     {
+      id:2,
       text:"ArtStation",
       href:"https://bhakim.artstation.com/",
     },
     {
+      id:3,
       text:"Itch.io",
       href:"https://badihaki.itch.io/",
     },
@@ -31,7 +36,7 @@ function Contact() {
           You can email me directly at <Anchor href='jojackblack@gmail.com' underline='hover' target='blank'>jojackblack@gmail.com</Anchor>.
         </p>
         
-        <div  className='ease-in-out transition-all duration-700 relative font-san bg-[#341e7a] bg-opacity-15 hover:bg-opacity-35 px-2 py-1 hover:py-4 my-4 rounded-lg hover:rounded-full w-1/2 hover:w-[75%] left-[25%] hover:left-[10%]'>
+        <div  className='flex flex-col ease-in-out transition-all duration-700 relative font-san bg-[#341e7a] bg-opacity-15 hover:bg-opacity-35 px-2 py-1 hover:py-4 my-4 rounded-lg hover:rounded-full w-1/2 hover:w-[75%] left-[25%] hover:left-[10%]'>
           <p>
             Alternatively, you can follow me on my socials:
           </p>
@@ -39,7 +44,7 @@ function Contact() {
             {
               socials.map(social=>{
                 return(
-                  <li>
+                  <li  key={social.id}>
                     <Anchor href={social.href} component={Link} target='blank'>
                       {social.text}
                     </Anchor>
