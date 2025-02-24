6ea3ab23c09ba0bcbeab19e12d4b553df6e8a1ab
badihaki <jojackblack@gmail.com>
2025-02-24 10:23:06 -0500

    more navbar styling to try to make sure link text moves correctly

--- src/lib/Components/Navigation/LinkComponent.tsx
+++ src/lib/Components/Navigation/LinkComponent.tsx
@@ -11,8 +11,9 @@ function LinkComponent(props: {
     return (
         <Link id={`link-${props.link.title.toLowerCase().replace(" ", "-")}`} href={props.link.href} onClick={props.buttonFunctionality}
         className={`transition-all ease-in-out m-auto px-11 text-center w-full rounded-full bg-gradient-to-t from-[#ffd900b0] from-5% to-transparent to-55%
-        ${props.menuIsOpen? "visible duration-100" : "hidden duration-1000"}`}>
-            <li className={`font-semibold tracking-wide text-xl`}>{props.link.title}</li>
+        ${props.menuIsOpen? "visible duration-300 w-fit" : "hidden duration-1000 w-0"}`}>
+            <li className={`transition-all ease-in-out duration-300 font-semibold tracking-wide text-xl
+                ${props.menuIsOpen ? "w-fit": "w-0"}`}>{props.link.title}</li>
         </Link>
     )
 }
