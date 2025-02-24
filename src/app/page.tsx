c9c85652d0593016c51aa41ab33e81d1f1df04d6
badihaki <jojackblack@gmail.com>
2025-02-24 00:18:17 -0500

    navigation styling. Nav bar now shows based on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 export default function Home() {
   return (
-    <main id="home" className="bg-blue-400 relative top-10 text-center p-2">
+    <main id="home" className="relative top-10 text-center p-2">
       <div id="home-container" className="flex flex-col gap-11">
-        <div id="home-intro" className="bg-green-600 relative w-fit mx-auto p-4 text-4xl font-semibold rounded-3xl">
+        <div id="home-intro" className="relative w-fit mx-auto p-4 text-4xl font-semibold rounded-3xl">
           Official Website of Badi-Haki Martin
         </div>
       </div>
 
       <div id="home-info" className="flex flex-col gap-4 mt-6">
-        <div id="home-details-1" className="bg-green-600 flex-1">
+        <div id="home-details-1" className="flex-1">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
-        <div id="home-details-2" className="bg-green-600 flex-1">
+        <div id="home-details-2" className="flex-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div id="home-details-3" className="flex-1">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
--- src/lib/Components/Navigation/NavBar.tsx
+++ src/lib/Components/Navigation/NavBar.tsx
@@ -24,14 +24,18 @@ function NavBar() {
   }
 
   return (
-    <nav id='navigation-menu' className='z-50 fixed bg-black h-screen w-[65%]'>
-      <Image id='menu-button' src={menuOpen ? openIcon : closedIcon} alt={`Open/Close navigation menu`} onClick={OnClickMenuButton} className={` transition-all ease-in-out duration-300 ${menuOpen ? `max-w-40` : `max-w-20`}`} />
-      <ul className='grid grid-rows-5 font-semibold tracking-wide text-xl '>
+    <nav id='navigation-menu' className={`z-50 fixed h-screen transition-all ease-in-out duration-300 ${menuOpen ? 'bg-black w-[65%]' : "bg-transparent w-0"}`}>
+      <Image id='menu-button' src={menuOpen ? openIcon : closedIcon} alt={`Open/Close navigation menu`} onClick={OnClickMenuButton} className={`transition-all ease-in-out p-2 relative rounded-full min-w-12
+        ${menuOpen ? `max-w-40 bg-transparent border-b-8 border-[#FFD700] top-2` : `max-w-20 bg-gradient-to-t from-[#FFD700]`} mx-auto top-2 duration-300`} />
+
+      <ul className={`grid grid-rows-5 font-semibold tracking-wide text-xl text-center h-1/2 relative top-[5%]
+        ${menuOpen ? "visible" : "hidden"}`}>
         <li>Webdev</li>
         <li>Gamedev</li>
         <li>Art</li>
         <li>Contact</li>
       </ul>
+
     </nav>
   )
 }
