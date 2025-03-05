af908f1fbed7576413c09a5221b9429df467bf4b
badihaki <jojackblack@gmail.com>
2025-03-04 20:34:41 -0500

    contact page has some content added to it

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,28 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
+import profile from '@/lib/Images/Profiles/profile-2025-orig.jpg'
+import email from '@/lib/Images/Icons/mail.png'
 
 function Contact() {
   return (
     <main id="contact" className="relative top-16 text-center p-2">
 
-    <div id="contact-container" className="flex flex-col gap-11">
-      <div id="contact-intro" className="relative w-fit mx-auto p-4 text-4xl font-semibold rounded-3xl">
-        contact
+      <div id="contact-container" className="flex flex-col mt-14">
+        <div id="contact-intro" className="relative bottom-11 w-fit mx-auto px-4 pt-4 text-4xl font-semibold rounded-3xl text-slate-200">
+          Contact Me
+        </div>
       </div>
-    </div>
 
-    <div id="contact-info" className="flex flex-col gap-4 mt-6">
-      <div id="contact-details-1" className="flex-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div id="contact-info-container" className="flex flex-col gap-4 md:gap-10 -mt-6 px-4 pt-2 pb-4 rounded-lg md:max-w-[70%] place-self-center">
+        <section id='contact-welcome' className='flex flex-col md:flex-row gap-4 w-[95%] place-self-center mx-auto place-items-center'>
+          <Image src={profile} alt='A profile picture of B. Haki Martin, in his office.' className='transition-all ease-in-out duration-500 relative w-[35%] hover:w-[50%] opacity-60 hover:opacity-85 active:opacity-100 rounded-full hover:rounded-md' />
+          <p id="contact-info-1" className="flex-1 outline outline-2 outline-slate-400 py-4 px-1 md:py-8 md:px-4 rounded-xl mt-2 indent-3">
+            <span className="font-semibold font-serif text-6xl border-b-2 border-[#FFD700] rounded-2xl relative bottom-1">T</span>
+            here are many ways to contact me.
+          </p>
+        </section>
       </div>
-      <div id="contact-details-2" className="flex-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-      <div id="contact-details-3" className="flex-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-    </div>
 
-  </main>
+      <button id='email' className='bg-slate-200 flex flex-row mx-auto rounded-full p-2'>
+        <Image src={email} alt='Email me!' className='place-self-start' />
+        <div className='text-slate-950 place-self-end'>Email me</div>
+      </button>
+
+    </main>
   )
 }
 
