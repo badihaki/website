00bfa0a6041f87bdefb100a4500e47efc9a24106
badihaki <jojackblack@gmail.com>
2025-03-13 10:30:59 -0400

    eslint unused error suppression 3

--- src/lib/Components/GameDev/ProjectPanelComponent.tsx
+++ src/lib/Components/GameDev/ProjectPanelComponent.tsx
@@ -13,7 +13,7 @@ function ProjectPanelComponent(props: {
     const { game, currentOpen, handlePanelClicked } = props;
     
     /* @typescript-eslint/no-unused-vars */
-    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const handleOnClick: MouseEventHandler<HTMLDivElement> = () => {
         if (canOpenPanel) {
             if (currentOpen === game.id) {
